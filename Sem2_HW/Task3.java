9699637f2497bf33bbee583bd8367670524a6bf6
Aleksey <alekc.col@mail.ru>
2023-08-02 11:57:29 +0300

    Add solution for Home Work Sem2

--- Sem2_HW/Task3.java
+++ Sem2_HW/Task3.java
@@ -1,5 +1,8 @@
 package Sem2_HW;
-
+/**
+ Если необходимо, исправьте данный код
+ (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+ */
 import java.io.FileNotFoundException;
 
 public class Task3 {
--- Sem2_HW/Task4.java
+++ Sem2_HW/Task4.java
@@ -1,7 +1,10 @@
 package Sem2_HW;
 
 import java.util.Scanner;
-
+/**
+ Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку. Пользователю должно
+ показаться сообщение, что пустые строки вводить нельзя.
+ */
 public class Task4 {
     public static void main(String[] args) {
         space();
